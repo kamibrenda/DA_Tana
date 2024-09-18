8f4b951b1b2de8de9b9d26b91094b617a3d018fc
Brenda Mukami <42267047+kamibrenda@users.noreply.github.com>
2024-09-18 18:35:24 +0300

    Built -n func insight

---
+++ python/vscodefiles/Built-in functions.py
@@ -0,0 +1,40 @@
+print('hello')
+
+print({'name':'Arnold',"age":45})
+
+list_num = [3,7,10]
+list_set = {3,7,10}
+list_tup = (3,7,10)
+
+sum(list_num)
+
+sum(list_tup)
+
+max(list_num)
+
+max(['a','b','z'])
+
+max('hello')
+min(list_tup)
+
+len(list_num)
+
+x = 100/3
+x
+round(x,2)
+
+x = range(0,11,2)
+tuple(x)
+
+
+sorted('hello')
+sorted([1,2,3,4,5,1])
+
+input()
+x = input()
+
+x = int(input())
+type(x)
+
+x = float(x)
+x
